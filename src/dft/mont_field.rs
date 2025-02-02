680e3d7a5b0da9c2cbb3c5d07e3ef87880f48383
Y5 <76672645+yugonsan@users.noreply.github.com>
2025-02-02 20:17:27 +0900

    m^-1, extended gcd

--- src/dft/mont_field.rs
+++ src/dft/mont_field.rs
@@ -23,6 +23,46 @@ impl MongomeryContext {
     }
 }
 
-fn inv_mod_u64(_a: u64, _m: u64) -> Option<u64> {
-    None
+/// モンテゴメリのmの逆元を求めるため
+fn inv_mod_u64(a: u64, m: u64) -> Option<u64> {
+    // gcd(a, m)とx,yを拡張ユークリッドで求める
+    // aが逆元を求めたい
+    // mがその時のmod
+    // gcdが1なら
+    // xがa^-1 mod bに該当する
+    let (gcd, x, _) = extended_gcd(a as i64, m as i64);
+
+    if gcd != 1 {
+        return None;
+    }
+
+    let mut x_mod_m = x % (m as i64);
+    if x_mod_m < 0 {
+        x_mod_m += m as i64;
+    }
+    Some(x_mod_m as u64)
+}
+
+/// gcd(a, b) = a*s0 + b*t0, 最終的に (gcd, si, ti) を返す
+fn extended_gcd(mut r0: i64, mut r1: i64) -> (i64, i64, i64) {
+    let (mut s0, mut s1) = (1, 0);
+    let (mut t0, mut t1) = (0, 1);
+
+    while r1 != 0 {
+        let q = r0 / r1;
+
+        let next_r = r0 - q * r1;
+        r0 = r1;
+        r1 = next_r;
+
+        let next_s = s0 - q * s1;
+        s0 = s1;
+        s1 = next_s;
+
+        let next_t = t0 - q * t1;
+        t0 = t1;
+        t1 = next_t;
+    }
+
+    (r0, s0, t0)
 }
